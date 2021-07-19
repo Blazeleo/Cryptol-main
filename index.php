dc6aec06c73ce02e82b6a002059cbc221040b013
Blazeleo <satviksams@gmail.com>
2021-07-19 12:56:57 +0530

    Final V 1.0

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     <div class="body-wrap">
         <nav class="navbar navbar-expand-lg navbar-light bg-light" style="position: fixed;">
             <img src="./images/Logo.png">
-            <a class="navbar-brand" href="./index.php">CRYPTOL</a>
+            <a class="navbar-brand" href="./landing.html">CRYPTOL</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent" style="display: flex;">
